8d766f31f3ec554ef206ceecbfafe930a03755e3
Stanisavljevic Ognjen <ognjen.stanisavljevic@thingsolver.com>
2023-09-11 22:35:35 +0200

    added function for checking edb bvk
    Signed-off-by: Stanisavljevic Ognjen <ognjen.stanisavljevic@thingsolver.com>

---
+++ edb-bvk/code/lambda_function.py
@@ -0,0 +1,39 @@
+import requests, re, os
+import smtplib
+from email.mime.text import MIMEText
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+sender = "ognjen.it@gmail.com"
+recipients = ["ognjen.it@gmail.com"]
+password = os.environ['SMTP_PASSWORD']
+ulice = "(БИХАЋК|БОЈЧИНСК|ОЛГЕ ЈОВИЧИЋ)"
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
+
+def check_sites():
+    for i in [1, 2, 3]:
+        r = requests.get(f"https://elektrodistribucija.rs/planirana-iskljucenja-beograd/Dan_{i}_Iskljucenja.htm", headers=headers)
+        r.encoding = r.apparent_encoding
+        result = re.search(ulice, r.text)
+        if result:
+            subject = "Alerting - EDB"
+            body = f"Bice ugasena struja za {i} dana. Patern {ulice}"
+            send_email(subject, body, sender, recipients, password)
+    r = requests.get("https://www.bvk.rs/planirani-radovi/", headers=headers)
+    r.encoding = r.apparent_encoding
+    result = re.search(ulice, r.text)
+    if result:
+            subject = "Alerting - BVK"
+            body = f"Bice ugasena vode za {i} dana. Patern {ulice}"
+            send_email(subject, body, sender, recipients, password)
+
+def lambda_handler(event, context):
+    return check_sites()
