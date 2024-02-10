775b8570f500e48b665f17be09bad14d5393d40c
Stanisavljevic Ognjen <ognjen.stanisavljevic@thingsolver.com>
2024-02-10 23:46:15 +0100

    added kubernetes support
    Signed-off-by: Stanisavljevic Ognjen <ognjen.stanisavljevic@thingsolver.com>

--- edb-bvk/code/lambda_function.py
+++ edb-bvk/code/lambda_function.py
@@ -16,10 +16,9 @@ def send_email(subject, body, sender, recipients, password):
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
        smtp_server.login(sender, password)
        smtp_server.sendmail(sender, recipients, msg.as_string())
-    print("Message sent!")
 
 def check_sites():
-    for i in [1, 2, 3]:
+    for i in [0, 1, 2, 3]:
         r = requests.get(f"https://elektrodistribucija.rs/planirana-iskljucenja-beograd/Dan_{i}_Iskljucenja.htm", headers=headers)
         r.encoding = r.apparent_encoding
         result = re.search(ulice, r.text)
@@ -27,13 +26,31 @@ def check_sites():
             subject = "Alerting - EDB"
             body = f"Bice ugasena struja za {i} dana. Patern {ulice}"
             send_email(subject, body, sender, recipients, password)
+            print("EDB - Message sent!")
+        else:
+            print("EDB - The message wasn't sent.")
     r = requests.get("https://www.bvk.rs/planirani-radovi/", headers=headers)
     r.encoding = r.apparent_encoding
     result = re.search(ulice, r.text)
     if result:
-            subject = "Alerting - BVK"
+            subject = "Alerting - BVK - planirani-radovi"
             body = f"Bice ugasena vode za {i} dana. Patern {ulice}"
             send_email(subject, body, sender, recipients, password)
+            print("BVK - Message sent!")
+    else:
+        print("BVK - The message wasn't sent.")
+    r = requests.get("https://www.bvk.rs/kvarovi-na-mrezi/", headers=headers)
+    r.encoding = r.apparent_encoding
+    result = re.search(ulice, r.text)
+    if result:
+            subject = "Alerting - BVK - kvarovi-na-mrezi"
+            body = f"Bice ugasena vode za {i} dana. Patern {ulice}"
+            send_email(subject, body, sender, recipients, password)
+            print("BVK - Message sent!")
+    else:
+        print("BVK - The message wasn't sent.")
 
-def lambda_handler(event, context):
-    return check_sites()
+check_sites()
+
+# def lambda_handler(event, context):
+#     return check_sites()
