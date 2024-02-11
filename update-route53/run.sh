54bc8bf23fabd8464dc161c8b126b1a31acd11e0
Stanisavljevic Ognjen <ognjen.stanisavljevic@thingsolver.com>
2024-02-11 02:04:14 +0100

    Added job
    Signed-off-by: Stanisavljevic Ognjen <ognjen.stanisavljevic@thingsolver.com>

--- update-route53/run.sh
+++ update-route53/run.sh
@@ -1,5 +1,3 @@
-
-
 is_ip_address() {
     local ip="$1"
     local ip_regex='^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
@@ -9,7 +7,7 @@ is_ip_address() {
 export NEW_ADDRESS=`curl http://ipconfig.io -s`
 export OLD_ADDRESS=`nslookup $DNS_RECORD | grep 'Address' | awk '{print $2}' | grep -v 53`
 
-# export NEW_ADDRESS="24.135.126.207"
+# export NEW_ADDRESS="192.168.0.14"
 # export OLD_ADDRESS="24.135.126.207"
 
 if is_ip_address "$NEW_ADDRESS"; then
